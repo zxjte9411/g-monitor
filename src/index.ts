2d61aeb05df93b4eb02b8a6e036cec1b9c413139
zxjte9411 <a5852241@gmail.com>
2026-01-28 00:50:57 +0800

    chore: prepare for npm publishing with nodejs compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env bun
+#!/usr/bin/env node
 import { Command } from 'commander';
 import { loginCommand } from './commands/login.js';
 import { statusCommand } from './commands/status.js';
